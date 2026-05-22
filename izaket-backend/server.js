a7c6d37083932234c515234992394f398b55c2e1
anas93-adminn <anas.93pbou@gmail.com>
2026-05-22 11:23:30 +0200

    cors fix test

--- izaket-backend/server.js
+++ izaket-backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "https://site-izaket.netlify.app",
+  origin: "*",
   credentials: true
 }));
 
