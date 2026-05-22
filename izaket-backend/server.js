936fc4778a543a12fddd73230f2922e85c32ebb1
anas93-adminn <anas.93pbou@gmail.com>
2026-05-22 12:26:25 +0200

    update

--- izaket-backend/server.js
+++ izaket-backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: "https://site-izaket.netlify.app",
+  origin: "https://site-izaket.netlify.app/",
   credentials: true
 }));
 
